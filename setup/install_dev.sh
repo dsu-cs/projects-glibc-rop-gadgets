5b9e40ab2a38387456bbb717489d28b933499138
Jonah Thiele <53587938+JonahThiele@users.noreply.github.com>
2025-10-15 10:57:51 -0500

    add setup script for python (#1)
    Co-authored-by: jonahdev <jonahthiele@yahoo.com>

---
+++ setup/install_dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Warning run this script from the setup dir or it will not work
+
+#create a virtual environment for python and load the libraries needed
+
+if [ ! -d "../venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv ../venv
+else
+    echo "Virtual environment already exists, skipping creation."
+fi
+
+source ../venv/bin/activate
+
+pip install -r requirements.txt
+
+# add any other commandline tools or dependencies that need to be added here
